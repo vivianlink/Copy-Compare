2bdd6bb7eb0e98be61ac3476e7b95ddba2582990
vivianlink <vvivian@sfu.ca>
2017-09-24 00:27:10 -0700

    first commit

---
+++ app/src/main/java/com/example/copycompare/copycompare/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.copycompare.copycompare;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickStart(View view){
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+}
