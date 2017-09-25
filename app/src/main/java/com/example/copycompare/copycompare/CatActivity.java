4c522a0bbbc717a562118cfc134a9335258e506e
vivianlink <vvivian@sfu.ca>
2017-09-24 21:22:25 -0700

    add cat image

--- app/src/main/java/com/example/copycompare/copycompare/CatActivity.java
+++ app/src/main/java/com/example/copycompare/copycompare/CatActivity.java
@@ -5,16 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cat);
     }
 
     public void clickStart(View view){
-        Intent intent = new Intent(this, CatActivity.class);
+        Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
 }
