fb56d093f0bc653586c1ceec21188060462d1148
vivianlink <vvivian@sfu.ca>
2017-09-24 21:07:06 -0700

    add canvas

--- app/src/main/java/com/example/copycompare/copycompare/GameActivity.java
+++ app/src/main/java/com/example/copycompare/copycompare/GameActivity.java
@@ -1,13 +1,24 @@
 package com.example.copycompare.copycompare;
 
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
+
+    private CanvasView customCanvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        customCanvas = (CanvasView) findViewById(R.id.drawing_canvas);
     }
+
+    public void clearCanvas(View v) {
+        customCanvas.clearCanvas();
+    }
+
 }
